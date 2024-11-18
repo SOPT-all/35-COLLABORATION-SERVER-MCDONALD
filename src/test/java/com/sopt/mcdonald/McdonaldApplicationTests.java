810ed75cf1023e87e36d05b60aae54c908dd7a25
yoonjuho <dbswn990@naver.com>
2024-11-18 11:53:14 +0900

    feat: 초기 프로젝트 세팅

---
+++ src/test/java/com/sopt/mcdonald/McdonaldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopt.mcdonald;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McdonaldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
