538a04e129147686668ad45643f3e91a7eda463f
yoonjuho <dbswn990@naver.com>
2024-11-18 21:53:20 +0900

    feat: Custom Exception 구현

---
+++ src/main/java/com/sopt/mcdonald/global/exception/McdonaldException.java
@@ -0,0 +1,8 @@
+package com.sopt.mcdonald.global.exception;
+
+public class McdonaldException extends RuntimeException {
+
+    public McdonaldException(String message) {
+        super(message);
+    }
+}
