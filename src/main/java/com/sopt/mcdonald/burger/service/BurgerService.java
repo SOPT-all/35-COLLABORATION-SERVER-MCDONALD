555d35d6b1a2f89d08a0ea9ee7b4700756474a9d
ivoryeee <psa109@sookmyung.ac.kr>
2024-11-26 23:39:34 +0900

    feat: favorites 조회 Service 구현

--- src/main/java/com/sopt/mcdonald/burger/service/BurgerService.java
+++ src/main/java/com/sopt/mcdonald/burger/service/BurgerService.java
@@ -1,12 +1,15 @@
 package com.sopt.mcdonald.burger.service;
 
 import com.sopt.mcdonald.burger.api.dto.response.BurgerResponse;
+import com.sopt.mcdonald.burger.api.dto.response.FavoriteListResponse;
 import com.sopt.mcdonald.burger.domain.BurgerEntity;
 import com.sopt.mcdonald.burger.repository.BurgerRepository;
 import com.sopt.mcdonald.global.exception.McdonaldException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Component
 public class BurgerService {
@@ -28,5 +31,11 @@ public class BurgerService {
         BurgerEntity burgerEntity = burgerRepository.findById(burgerId)
                 .orElseThrow(()-> new McdonaldException("버거 id를 찾을 수 없습니다.", HttpStatus.NOT_FOUND));
         burgerEntity.switchLiked();
+    }
+
+    @Transactional(readOnly = true)
+    public FavoriteListResponse getLikeList() {
+        List<BurgerEntity> burgers = burgerRepository.findByIsLikedTrue();
+        return FavoriteListResponse.of(burgers);
     }
 }
